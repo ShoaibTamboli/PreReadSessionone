4e230d42b9ceb79e4ade3ab6606c8aad8bf823b0
ShoaibTamboli <shoaibtamboli03@gmail.com>
2022-01-08 19:55:23 +0530

    update

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ function submit(){
 
 //Arrow Function
 
-x = 3.14;       // This will not cause an error.
-myFunction();
-
-function myFunction() {
-  "use strict";
-  y = 3.14;   // This will cause an error
-}
+let x=5;
+console.log(x);
 
